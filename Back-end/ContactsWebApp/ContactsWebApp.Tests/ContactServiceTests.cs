0458f588c3a2b462a121a1b2e517b525d4780530
minmck <mindaugas.mcks@gmail.com>
2022-02-06 21:11:37 +0200

    Unit tests.

---
+++ Back-end/ContactsWebApp/ContactsWebApp.Tests/ContactServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using ContactsWebApp.BLL.Repository;
+using ContactsWebApp.BLL.Services;
+using ContactsWebApp.Shared.Entities;
+using Moq;
+using Xunit;
+
+namespace ContactsWebApp.Tests
+{
+    public class ContactServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _mockRepo;
+        private readonly ContactService _contactService;
+
+        public ContactServiceTests()
+        {
+            _mockRepo = new Mock<IUnitOfWork>();
+            _contactService = new ContactService(_mockRepo.Object);
+        }
+
+        [Fact]
+        public async void CreateNewContactAsync_CallsCreateMethodOneTime()
+        {
+            var contact = GetContact();
+            var userId = 1;
+            _mockRepo.Setup(x => x.Contact.CreateNewContact(contact));
+
+            await _contactService.CreateNewContactAsync(userId, contact);
+
+            _mockRepo.Verify(x => x.Contact.CreateNewContact(contact), Times.Exactly(1));
+        }
+
+        [Fact]
+        public async void DeleteContactAsync_CallsDeleteMethodOneTime()
+        {
+            var contact = GetContact();
+            _mockRepo.Setup(x => x.Contact.DeleteContact(contact));
+
+            await _contactService.DeleteContactAsync(contact);
+
+            _mockRepo.Verify(x => x.Contact.DeleteContact(contact), Times.Exactly(1));
+        }
+
+        [Fact]
+        public async void UpdateContactAsync_CallsUpdateMethodOneTime()
+        {
+            var id = 1;
+            var userId = 1;
+            var contact = GetContact();
+            _mockRepo.Setup(x => x.Contact.UpdateContact(contact));
+
+            await _contactService.UpdateContactAsync(id, userId, contact);
+
+            _mockRepo.Verify(x => x.Contact.UpdateContact(contact), Times.Exactly(1));
+        }
+
+        private Contact GetContact()
+        {
+            var contact = new Contact()
+            {
+                Id = 1,
+                FullName = "Vardenis Pavardenis",
+                Email = "vardenis@email.com",
+                PhoneNumber = "123456789"
+            };
+            return contact;
+        }
+    }
+}
---
+++ Back-end/ContactsWebApp/ContactsWebApp.Tests/UserServiceTests.cs
@@ -0,0 +1,116 @@
+﻿using Autofac.Extras.Moq;
+using ContactsWebApp.BLL.Repository;
+using ContactsWebApp.BLL.Services;
+using ContactsWebApp.Shared.Entities;
+using Moq;
+using Xunit;
+
+namespace ContactsWebApp.Tests
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _mockRepo;
+        private readonly UserService _userService;
+
+        public UserServiceTests()
+        {
+            _mockRepo = new Mock<IUnitOfWork>();
+            _userService = new UserService(_mockRepo.Object);
+        }
+
+        [Fact]
+        public async void UserExistsAsync_UserExists_ReturnsTrue()
+        {
+            var user = GetUser();
+            var email = "email@email.com";
+            _mockRepo.Setup(x => x.User.FindUserByEmailAsync(email))
+                .ReturnsAsync(user);
+
+            var actual = await _userService.UserExistsAsync(email);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public async void UserExistsAsync_UserDoesNotExist_ReturnsFalse()
+        {
+            User user = null;
+            var email = "email@email.com";
+            _mockRepo.Setup(x => x.User.FindUserByEmailAsync(email))
+                .ReturnsAsync(user);
+
+            var actual = await _userService.UserExistsAsync(email);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public async void CreateNewUserAsync_CallsCreateMethodOneTime()
+        {
+            var user = GetUser();
+            _mockRepo.Setup(x => x.User.CreateNewUser(user));
+
+            await _userService.CreateNewUserAsync(user);
+
+            _mockRepo.Verify(x => x.User.CreateNewUser(user), Times.Exactly(1));
+        }
+
+        [Fact]
+        public async void UserValidAsync_UserExists_ReturnsTrue()
+        {
+            var user = GetUser();
+            var email = "email@email.com";
+            var password = "password";
+            _mockRepo.Setup(x => x.User.FindUserByEmailAndPasswordAsync(email, password))
+                .ReturnsAsync(user);
+
+            var actual = await _userService.UserValidAsync(email, password);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public async void UserValidAsync_UserDoesNotExist_ReturnsFalse()
+        {
+            User user = null;
+            var email = "email@email.com";
+            var password = "password";
+            _mockRepo.Setup(x => x.User.FindUserByEmailAndPasswordAsync(email, password))
+                 .ReturnsAsync(user);
+
+            var actual = await _userService.UserValidAsync(email, password);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public async void FindUserByEmailAsync_ReturnsUser()
+        {
+            var email = "email@email.com";
+            var user = GetUser();
+            _mockRepo.Setup(x => x.User.FindUserByEmailAsync(email))
+                .ReturnsAsync(user);
+
+            var expected = GetUser();
+            var actual = await _userService.FindUserByEmailAsync(email);
+
+            Assert.True(actual != null);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+        }
+
+        private User GetUser()
+        {
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "john@email.com",
+                Password = "password"
+            };
+            return user;
+        }
+    }
+}
